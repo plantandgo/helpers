3ba72088dd4281f14e9469d4334785ec92b85fc3
Juraj Piovar <jpiovar@sk.ibm.com>
2021-07-27 13:21:20 +0200

    update

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
-// function isExplorer() {
-//   return window.navigator.userAgent.indexOf('MSIE ') > -1 || !!window.navigator.userAgent.match(/Trident.*rv\:11\./);
-// }
-
-// // function getItemBasedId(arr, id) {
-// //   return arr.filter((item) => item.id === id)[0];
-// // }
-
-// // function getIndexBasedId(arr, id) {
-// //   // debugger;
-// //   return arr.findIndex(item => item.id === id);
-// // }
-
-// module.exports = isExplorer;
-
-'use strict';
- 
-function add(a, b) {
-    return a + b;
+function isExplorer() {
+  return window.navigator.userAgent.indexOf('MSIE ') > -1 || !!window.navigator.userAgent.match(/Trident.*rv\:11\./);
 }
- 
-function subtract(a, b) {
-    return a - b;
+
+function getItemBasedId(arr, id) {
+  return arr.filter((item) => item.id === id)[0];
 }
- 
-function divide(a, b) {
-    return a / b;
-}
- 
-function multiply(a, b) {
-    return a * b;
+
+function getIndexBasedId(arr, id) {
+  // debugger;
+  return arr.findIndex(item => item.id === id);
 }
 
 module.exports = {
-  add,
-  subtract,
-  divide,
-  multiply
+    isExplorer,
+    getItemBasedId,
+    getIndexBasedId
 }
