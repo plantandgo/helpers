cc3c4bf49fa82be051745e61685920e93ef2f96a
Juraj Piovar <jpiovar@sk.ibm.com>
2021-07-27 11:49:49 +0200

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+function isExplorer() {
+  return window.navigator.userAgent.indexOf('MSIE ') > -1 || !!window.navigator.userAgent.match(/Trident.*rv\:11\./);
+}
+
+// function getItemBasedId(arr, id) {
+//   return arr.filter((item) => item.id === id)[0];
+// }
+
+// function getIndexBasedId(arr, id) {
+//   // debugger;
+//   return arr.findIndex(item => item.id === id);
+// }
+
+module.exports = isExplorer;
