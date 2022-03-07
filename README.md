d8169b2b2c851b0d126cc34c57b424f5f6ce988a
Juraj Piovar <jpiovar@sk.ibm.com>
2022-03-07 10:28:51 +0100

    update

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 isExplorer - returns true/false based on recognition<br/>
 getItemBasedId - returns item of object based it's id property<br/>
 getIndexBasedId - returns index of object based it's id property<br/>
+isLocalHost - returns true in case is Localhost or false in case is not localhost<br/>
--- index.js
+++ index.js
@@ -11,8 +11,16 @@ function getIndexBasedId(arr, id) {
   return arr.findIndex(item => item.id === id);
 }
 
+function isLocalHost() {
+  if (['localhost', '127.0.0.1', ''].includes(window.location.hostname)) {
+    return true;
+  }
+  return false;
+}
+
 module.exports = {
     isExplorer,
     getItemBasedId,
-    getIndexBasedId
+    getIndexBasedId,
+    isLocalHost
 }
